efa63be986b4e74e6d9140e5dd89fa8a8e45d50e
Ashutosh Ranjan <authran.off@gmail.com>
2026-02-10 20:25:46 +0530

    Setting up for paytm project

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+// Load env file
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
