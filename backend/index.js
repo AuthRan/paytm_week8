9948a1f80d687c1e201143b286fb92feab2e59d9
Ashutosh Ranjan <authran.off@gmail.com>
2026-03-03 22:36:16 +0530

    Completed everything before step 10

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const mongoose = require("mongoose")
+const mainRouter = require("./routes/index")
+const cors = require("cors")
 // Load env file
 dotenv.config();
 
+app.use(cors())
+app.use(express.json())
+
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -13,6 +18,8 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+
+app.use("/api/v1", mainRouter)
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
